8dddefc07361e4736a53c6a57faeff24238c139f
sergio <a18074@alunos.ipca.pt>
2024-02-07 17:35:54 +0000

    Work Done: - Building into the code the Animations library to animate images on SlashActivity.
    * Notes:
    -...
    
    * Add items:
    - Animations.java
    
    * Updated items:
    - build.gradle.kts
    - SplashActivity.java
    
    * Removed:
    -...

--- app/src/main/java/exemple/weatherapp/api/java/yupweather/SplashActivity.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/SplashActivity.java
@@ -3,12 +3,45 @@ package exemple.weatherapp.api.java.yupweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationSet;
+import android.widget.ImageView;
+
+import exemple.weatherapp.api.java.yupweather.databinding.ActivitySplashBinding;
+import exemple.weatherapp.api.java.yupweather.utilities.Animations;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    private ImageView imageViewSun, imageViewCloud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        components();
+
+        Animations animations = new Animations();
+
+        AnimationSet multipleSunAnimationSet = new AnimationSet(true);
+        multipleSunAnimationSet.addAnimation(animations.getAnimFade());
+        multipleSunAnimationSet.addAnimation(animations.getRotateAnim());
+        imageViewSun.startAnimation(multipleSunAnimationSet);
+
+        AnimationSet multipleCloudAnimationSet = new AnimationSet(true);
+        multipleCloudAnimationSet.addAnimation(animations.getAnimFade());
+        multipleCloudAnimationSet.addAnimation(animations.getMovementAnimation());
+        imageViewCloud.startAnimation(multipleCloudAnimationSet);
+
+
+    }
+
+    private void components() {
+        imageViewSun =  binding.imageViewSun;
+        imageViewCloud = binding.imageViewCloud;
     }
 }
