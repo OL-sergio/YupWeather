c60b2f16675ff55ee13436b343b400e67bed4409
sergio <a18074@alunos.ipca.pt>
2024-02-07 17:25:07 +0000

    Work Done: - Building the custom splash activity on XML by adding resources and components
    * Notes:
    -...
    
    * Add items:
    - activity_splash.xml
    - background_detail.png
    - ic_cloud
    - ic_clear_day.xml
    - SplashActivity
    
    * Updated items:
    - activity_main.xml
    - AndroidManifest.xml
    - MainActivity
    
    * Removed:
    -...

--- app/src/main/java/exemple/weatherapp/api/java/yupweather/MainActivity.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/MainActivity.java
@@ -1,14 +1,23 @@
 package exemple.weatherapp.api.java.yupweather;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text_test);
+
+        textView.setText("adadadad");
     }
 }
