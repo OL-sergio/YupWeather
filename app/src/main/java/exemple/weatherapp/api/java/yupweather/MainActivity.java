75827c9dac6466acff1abfbc32796a703db84c91
sergio <a18074@alunos.ipca.pt>
2024-02-08 16:38:02 +0000

    Work Done: - Applying the toolbar feature and adding and updating the necessary resources in the toolbar.
    * Notes:
    -...
    
    * Add items:
    - ic_gps_fixed_24.xml
    - ic_search_24.xml
    - menu_main.xml
    
    * Updated items:
    - MainActivity
    - activity_main.xml
    - SystemUi.java
    
    * Removed:
    -...

--- app/src/main/java/exemple/weatherapp/api/java/yupweather/MainActivity.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/MainActivity.java
@@ -1,23 +1,65 @@
 package exemple.weatherapp.api.java.yupweather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import exemple.weatherapp.api.java.yupweather.databinding.ActivityMainBinding;
+import exemple.weatherapp.api.java.yupweather.utilities.SystemUi;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        TextView textView = (TextView) findViewById(R.id.textView_todayWeather);
+        SystemUi systemUi = new SystemUi();
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(systemUi.settingsMainNavigation());
+
+        Toolbar toolbarMain = binding.toolbarMain.toolbar;
+        setSupportActionBar(toolbarMain);
+
+        TextView textView = findViewById(R.id.textView_todayWeather);
 
         textView.setText("adadadad");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menuItem_gpsLocation) {
+            // Handle click event for menu item 1
+            return true;
+        } else if (item.getItemId() == R.id.menuItem_searchLocation) {
+            // Handle click event for menu item 2
+            Log.d("Data: ", "search");
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/exemple/weatherapp/api/java/yupweather/utilities/SystemUi.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/utilities/SystemUi.java
@@ -15,4 +15,14 @@ public class SystemUi {
         return uiOptions;
     }
 
+    public int settingsMainNavigation(){
+        uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                //| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                //| View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        return uiOptions;
+    }
+
 }
