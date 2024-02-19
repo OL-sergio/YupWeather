26367b9cb791a9e0e9d096bfac57af5ec43ae4ec
sergio <a18074@alunos.ipca.pt>
2024-02-19 17:42:12 +0000

    Work Done: - Applying the GPS Tracker that retrieves the location of the user by latitude and longitude.
    * Notes:
    -...
    
    * Add items:
    - Constants
    - GPSTracker
    
    * Updated items:
    - AndroidManifest.xml
    - MainActivity.java
    
    * Removed:
    -...

--- app/src/main/java/exemple/weatherapp/api/java/yupweather/MainActivity.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/MainActivity.java
@@ -1,6 +1,7 @@
 package exemple.weatherapp.api.java.yupweather;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,14 +13,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import exemple.weatherapp.api.java.yupweather.databinding.ActivityMainBinding;
+import exemple.weatherapp.api.java.yupweather.utilities.Constants;
+import exemple.weatherapp.api.java.yupweather.utilities.GPSTracker;
 import exemple.weatherapp.api.java.yupweather.utilities.SystemUi;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private TextView dateTimeDay, temperatureDay, todayWeather,
+            visibilityDay, pressureDay, windDay, humidityDay;
+
+    private GPSTracker gpsTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +45,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbarMain = binding.toolbarMain.toolbar;
         setSupportActionBar(toolbarMain);
 
+        components();
+
         TextView textView = findViewById(R.id.textView_todayWeather);
 
         textView.setText("adadadad");
+
+        try {
+            if ( ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION )
+                    != PackageManager.PERMISSION_GRANTED ){
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION}, Constants.requestCode );
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        getLocationData();
     }
 
     @Override
@@ -47,11 +72,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-
-
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         if (item.getItemId() == R.id.menuItem_gpsLocation) {
+            getLocationData();
             // Handle click event for menu item 1
             return true;
         } else if (item.getItemId() == R.id.menuItem_searchLocation) {
@@ -61,5 +85,31 @@ public class MainActivity extends AppCompatActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void getLocationData(){
+        gpsTracker = new GPSTracker(MainActivity.this );
+        if (gpsTracker.statusSettingsLocation()){
+            String latitude = String.valueOf(gpsTracker.getLatitude());
+            String longitude = String.valueOf(gpsTracker.getLongitude());
+            Log.d("Location: ", latitude + " , " + longitude );
+
+        }
+    }
+
+    private void components() {
+        dateTimeDay = binding.textViewDateForecast;
+        temperatureDay = binding.textViewTodayTemperature;
+        todayWeather = binding.textViewTodayWeather;
+        visibilityDay = binding.textViewDataVisibility;
+        pressureDay = binding.textViewDataAirPressure;
+        windDay = binding.textViewDataWind;
+        humidityDay = binding.textViewDataHumidity;
+    }
+
+    @Override
+    protected void onDestroy() {
+        gpsTracker.stopUsingGPS();
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/utilities/Constants.java
@@ -0,0 +1,5 @@
+package exemple.weatherapp.api.java.yupweather.utilities;
+
+public final class Constants {
+    public static final int requestCode = 101 ;
+}
