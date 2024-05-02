8c47555ce5a24ea4ad9aa5a1c47f4ba04b114056
sergio <a18074@alunos.ipca.pt>
2024-05-02 16:20:38 +0100

    Work Done: - Organizing the location of classes on the project.
    * Notes:
    - ...
    
    * Add items:
    - ...
    
    * Updated items:
    - DataServiceConditions
    - DataServiceDaily
    - DataServiceDaily
    
    * Removed:
    - ...

--- app/src/main/java/exemple/weatherapp/api/java/yupweather/database/api/service/DataServiceConditions.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/database/api/service/DataServiceConditions.java
@@ -1,14 +1,13 @@
 package exemple.weatherapp.api.java.yupweather.database.api.service;
 
 
-import exemple.weatherapp.api.java.yupweather.model.WeatherConditionsDay;
+import exemple.weatherapp.api.java.yupweather.model.forecastday.WeatherConditionsDay;
 import exemple.weatherapp.api.java.yupweather.utilities.Constants;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 //https://pro.openweathermap.org/data/2.5/weather?lat=37&lon=-122&appid=apikey
-
 
 public interface DataServiceConditions {
 
--- app/src/main/java/exemple/weatherapp/api/java/yupweather/database/api/service/DataServiceDaily.java
+++ app/src/main/java/exemple/weatherapp/api/java/yupweather/database/api/service/DataServiceDaily.java
@@ -1,13 +1,14 @@
 package exemple.weatherapp.api.java.yupweather.database.api.service;
 
 
-import exemple.weatherapp.api.java.yupweather.model.forescastdaily.DailyResponse;
+import exemple.weatherapp.api.java.yupweather.model.forecastdaily.DailyResponse;
 import exemple.weatherapp.api.java.yupweather.utilities.Constants;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 //https://pro.openweathermap.org/data/2.5/forecast/daily?lat=37&lon=-122&cnt=4&appid=apikey&units=metric
+
 public interface DataServiceDaily {
     @GET(Constants.DAILY)
     Call<DailyResponse> getDailyWeatherConditions (
